2a62af2d93255835b8b3d55f4b3a09ab40fbba5d
jdevo23 <jrdeveson@hotmail.com>
2019-06-03 17:37:23 +0100

    change database connection config

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const connection = mysql.createConnection({
   host: keys.host,
   user: keys.user,
   password: keys.password,
-  database: keys.database
+  database: keys.database,
+  port: '3306'
 })
 
-connection.connect()
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
 
 const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) => {
   res.send('hello world')
-})
-
-app.listen(PORT, () => {
-  console.log(`App listening on port: ${PORT}`)
 })
