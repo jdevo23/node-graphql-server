972b786c70af56aa90c9a01d3537f782e0d9a197
jdevo23 <jrdeveson@hotmail.com>
2019-06-21 17:25:55 +0100

    add column to table

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const mysql = require('mysql')
+const keys = require('./keys')
+
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./src/schema');
 
@@ -6,3 +10,45 @@ const server = new ApolloServer({ typeDefs });
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
+
+const connection = mysql.createConnection({
+  host: keys.host,
+  user: keys.user,
+  password: keys.password,
+  database: keys.database,
+  port: '3306'
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  const createTable = "CREATE TABLE motorcycles (? VARCHAR(255), ? VARCHAR(255))"
+  const createRow = "INSERT INTO motorcycles(make, model) VALUES(?, ?)"
+  const createMultipleRows = "INSERT INTO motorcycles(make, model) VALUES ?"
+  const selectAll = "SELECT * FROM motorcycles"
+  const update = `UPDATE motorcycles 
+    SET id = ?
+    WHERE model = ?`
+  const remove = `DELETE FROM motorcycles where model = ?`
+  
+  connection.query(selectAll, (err, res, fields) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(res)
+  })
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+// connection.end();
+
+const app = express()
+const PORT = 5000
+
+// app.get('/', (req, res) => {
+//   res.send('hello world')
+// })
